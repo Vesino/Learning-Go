a9c212301657736e301c77cd9bae0cc1d07d7bcc
Luis Vargas <luisvargasr10@gmail.com>
2021-11-15 20:12:28 -0600

    Understand Array & Slices

--- chapter1/main.go
+++ chapter1/main.go
@@ -2,12 +2,18 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	i := 1
+// 	for i <= 10 {
+// 		fmt.Println(i)
+// 		i = i + 1
+// 	}
+// }
+
 func main() {
-	fmt.Print("Enter a number: ")
-	var input float64
-	fmt.Scanf("%f", &input)
-
-	output := input * 2
-
-	fmt.Println(output)
+	slice1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	slice2 := make([]int, 8)
+	copy(slice2, slice1)
+	fmt.Println(slice1, slice2)
+	fmt.Println(slice1[0])
 }
