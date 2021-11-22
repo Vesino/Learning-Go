739ae62465f3bc8147fb568e92ab1001761ef45e
Luis Vargas <luisvargasr10@gmail.com>
2021-11-21 21:46:30 -0600

    Maps in Go (Dictionaries)

--- chapter1/main.go
+++ chapter1/main.go
@@ -10,10 +10,95 @@ import "fmt"
 // 	}
 // }
 
+// func main() {
+// 	slice1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+// 	slice2 := make([]int, 8)
+// 	copy(slice2, slice1)
+// 	fmt.Println(slice1, slice2)
+// 	fmt.Println(slice1[0])
+// }
+
+// MAPS
 func main() {
-	slice1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	slice2 := make([]int, 8)
-	copy(slice2, slice1)
-	fmt.Println(slice1, slice2)
-	fmt.Println(slice1[0])
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Fluorine"
+	elements["Ne"] = "Neon"
+
+	fmt.Println(elements["Li"])
+	name, ok := elements["Un"] // Unexisten key
+	fmt.Println(name, ok)      // This print false
+
+	if name, ok := elements["Ne"]; ok {
+		fmt.Println(name, ok)
+	}
+
+	// There is a shorter way in order to create maps.
+	elementos := map[string]string{
+		"H":  "Hydrogen",
+		"He": "Helium",
+		"Li": "Lithium",
+		"Be": "Beryllium",
+		"B":  "Boron",
+		"C":  "Carbon",
+		"N":  "Nitrogen",
+		"O":  "Oxygen",
+		"F":  "Fluorine",
+		"Ne": "Neon",
+	}
+	fmt.Println(elementos)
+
+	// Create a map of maps
+	new_elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+		"Be": map[string]string{
+			"name":  "Beryllium",
+			"state": "solid",
+		},
+		"B": map[string]string{
+			"name":  "Boron",
+			"state": "solid",
+		},
+		"C": map[string]string{
+			"name":  "Carbon",
+			"state": "solid",
+		},
+		"N": map[string]string{
+			"name":  "Nitrogen",
+			"state": "gas",
+		},
+		"O": map[string]string{
+			"name":  "Oxygen",
+			"state": "gas",
+		},
+		"F": map[string]string{
+			"name":  "Fluorine",
+			"state": "gas",
+		},
+		"Ne": map[string]string{
+			"name":  "Neon",
+			"state": "gas",
+		},
+	}
+	if lithium, ok := new_elements["Li"]; ok {
+		fmt.Println(lithium["name"], lithium["state"])
+	}
 }
